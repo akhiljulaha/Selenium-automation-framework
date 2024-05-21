e674f61660854131073ff34d08a1544bd13196eb
unknown <you@example.com>
2024-05-21 23:57:16 +0530

    feature 2

---
+++ src/main/java/com/qa/opencart/pages/Feature2.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+import javax.swing.plaf.synth.SynthGraphicsUtils;
+
+public class Feature2 {
+
+	public static void main(String[] args) {
+		System.out.println("Feature 2");
+	}
+
+}
